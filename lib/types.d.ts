79ec6748b6d2f96fe14b8927b836d92bdf94ef3d
apaulho <76706473+apaulho@users.noreply.github.com>
2025-10-13 13:13:10 -0400

    Revert "Fix TypeScript module resolution error for neon-db"
    This reverts commit a40154581590d484d2d29500c513bddb25d2789c.

--- lib/types.d.ts
+++
@@ -1,8 +0,0 @@
-// lib/types.d.ts - Type declarations for external modules
-declare module '../../../lib/neon-db' {
-  import { Pool } from 'pg';
-  const pool: Pool;
-  export default pool;
-  export { pool };
-  export function query(text: string, params?: any[]): Promise<any>;
-}
